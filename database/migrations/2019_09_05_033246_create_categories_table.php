8049a8c8b27e76709f1e6463dc0b579130283631
carlosocarvalho <>
2019-09-05 00:39:27 -0300

    add categorie

--- database/migrations/2019_09_05_033246_create_categories_table.php
+++ database/migrations/2019_09_05_033246_create_categories_table.php
@@ -15,6 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->text('description')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
